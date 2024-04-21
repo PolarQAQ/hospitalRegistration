80ad993e81f40e80e47f1e71f0d50c28ba25e933
yangmy <17550230061@163.com>
2024-04-21 11:13:30 +0800

    测试项目是否构建完成

---
+++ src/main/java/registration/hospitalregistration/HospitalRegistrationApplication.java
@@ -0,0 +1,13 @@
+package registration.hospitalregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalRegistrationApplication.class, args);
+    }
+
+}
